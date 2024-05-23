66a91c4009a199900e75120673b0c3f6ae2c073c
John Shrubb <john-shrubb@users.noreply.github.com>
2024-05-23 12:29:13 +0100

    Set up index.ts and a dbConnection that can be used to grab a connection to the database

---
+++ src/bugtrack-core/dbConnection.ts
@@ -0,0 +1,11 @@
+import pg from 'pg';
+import { hostname, port, password, database, username } from '../config/dbParameters.json';
+
+const clientPool = new pg.Pool({
+	host: hostname,
+	port: port,
+	password: password,
+	database: database,
+	user: username,
+}).connect();
+export default await clientPool;
