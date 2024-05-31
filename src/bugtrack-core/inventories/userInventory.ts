91859db772fc2191164d6ca32e22f93c2235d812
John Shrubb <john-shrubb@users.noreply.github.com>
2024-05-31 17:25:56 +0100

    Update user inventory with better validation and commenting.

--- src/bugtrack-core/inventories/userInventory.ts
+++ src/bugtrack-core/inventories/userInventory.ts
@@ -118,7 +118,11 @@ class UserInventory {
 		// Grab all of the user data from the view.
 		const allUserData : QueryResult<userRowStruct> = await this.gpPool.query(
 			'SELECT userid, username, email, displayname, pfp, creationdate FROM usersgp;',
-		);
+		).catch((reason) => {
+			const error = new Error('Failed to initialise user cache.');
+			error.cause = reason || 'Call to PostgreSQL failed for unknown reason.';
+			throw error;
+		});
 
 		// For each row in the user data...
 		allUserData.rows.forEach(userData => {
@@ -169,9 +173,13 @@ class UserInventory {
 	 * @param userEmail 
 	 */
 	public getUserByEmail(userEmail : string) : User | null {
+		// For each item in the user cache...
 		for (const key in this.userMap) {
+			// Get the user object + Non null assertion as cache can be trusted.
 			const user : User = this.userMap.get(key)!;
+			// Check if the email matches.
 			if (user.email === userEmail) {
+				// Return if it does.
 				return user;
 			}
 		}
@@ -187,6 +195,8 @@ class UserInventory {
 	 * @param username 
 	 */
 	public getUserByUsername(username : string) : User | null {
+		// getUserByEmail works in much of the same way except it gets a user by a
+		// different field.
 		for (const key in this.userMap) {
 			const user : User = this.userMap.get(key)!;
 			if (user.username === username) {
@@ -209,23 +219,91 @@ class UserInventory {
 	 */
 
 	public async updateUser(user : User, updateType : UserUpdateType, newValue : string) {
+		// This method cannot be used to update a password.
 		if (updateType === UserUpdateType.password) {
 			const error = new TypeError('Passed invalid argument for updateType. Passwords must be updated via userManagerInventory.');
+			error.cause = updateType;
 			throw error;
 		}
 
+		// Trim the new value they want to set.
+		// Constraints for parameters are as follows:
+		// 3 <= username    <= 30
+		// 6 <= email       <= 256
+		// 3 <= displayname <= 50
+		// PFP is exempt as it is a URL.
 		newValue = newValue.trim();
 
+		// Pass a seperate error if an empty string is passed in.
+		// A cause isn't really necessary as it is perfectly assumable what was passed to
+		// cause this error.
 		if (newValue.length === 0) {
 			const error = new Error('Cannot pass an empty string for newValue argument.');
 			throw error;
 		}
 
+		// Length validation
+
+		// username validation
+		if (
+			updateType === UserUpdateType.username &&
+			(newValue.length < 3 || newValue.length > 30)
+		) {
+			// Build a cause which makes the issue more debuggable if something goes
+			// wrong.
+			const cause = {
+				enumerator: updateType.toString(),
+				passedValue: newValue,
+			};
+			
+			// Build and throw error.
+			const error = new Error('Invalid format for username. Must be >= 3 || <= 30.');
+			error.cause = cause;
+			throw error;
+		}
+
+		// displayname validation
+		if (
+			updateType === UserUpdateType.displayname &&
+			(newValue.length < 3 || newValue.length > 50)
+		) {
+			// Cause
+			const cause = {
+				enumerator: updateType.toString(),
+				passedValue: newValue,
+			};
+
+			// Error
+			const error = new Error('Invalid format for displayname. Must be >= 3 || <= 50.');
+			error.cause = cause;
+			throw error;
+		}
+
+		// email validation
+		// Similar/identical structure to above.
+		if (
+			updateType === UserUpdateType.email &&
+			(newValue.length < 6 || newValue.length > 256)
+		) {
+			// Cause
+			const cause = {
+				enumerator: updateType.toString(),
+				passedValue: newValue,
+			};
+
+			// Error
+			const error = new Error('Invalid format for email. Must be >= 6 || <= 256.');
+			error.cause = cause;
+			throw error;
+		}
+
+		// Check if the user exists and throw an error if it does not.
 		if (!this.getUserByID(user.id)) {
 			const error = new Error('Could not find a user with ID ' + user.id);
 			throw error;
 		}
 
+		// Mapping to map the enumerator to the column name in the database.
 		const columnUpdateMapping = {
 			[UserUpdateType.displayname]: 'displayname',
 			[UserUpdateType.email]: 'email',
@@ -233,9 +311,36 @@ class UserInventory {
 			[UserUpdateType.pfp]: 'pfp',
 		};
 		
+		// Utilise column mapping by making a string with the appropriate column name in
+		// it.
 		const columnToUpdate = columnUpdateMapping[updateType];
 
-		this.gpPool.query(`UPDATE usersgp SET ${columnToUpdate}=$1 WHERE userid=$2;`);
+		// I swear this should be secure, right? What could go wrong with SQL and string
+		// interpolation????
+		const query = `UPDATE usersgp SET ${columnToUpdate}=$1 WHERE userid=$2;`;
+		
+		// Query the database.
+		await this.gpPool.query(
+			query,
+			[newValue, user.id]
+		// And catch just to throw another more debuggable error out if something goes
+		// wrong.
+		).catch((reason) => {
+			// Build a cause object with more details than otherwise.
+			const cause = {
+				rejectionReason: reason,
+				updateType: updateType.toString(),
+				query: query,
+				newValString: newValue,
+			};
+
+			// Build the error with the cause.
+			const error = new Error('Error updating user details. Check cause for more details.');
+			error.cause = cause;
+
+			// Throw the error.
+			throw error;
+		});
 
 		this.bugtrackCore.cacheInvalidation.notifyUpdate(possibleEvents.user, user.id);
 	}
