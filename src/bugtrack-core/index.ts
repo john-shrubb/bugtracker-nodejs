95f41976737c838f0f5ef77c566e90380ff0e760
John Shrubb <john-shrubb@users.noreply.github.com>
2024-05-31 11:18:34 +0100

    Add import for general purpose role for database to be passed around.

--- src/bugtrack-core/index.ts
+++ src/bugtrack-core/index.ts
@@ -1,10 +1,24 @@
 import { CacheInvalidationService } from './services/cacheInvalidationService.js';
+import { gpPool } from './dbConnection.js';
+
+/**
+ * The core of the bug tracker exposes all required services which other parts of the
+ * overall app can interact with such as the web API.
+ */
 
 class BugtrackCore {
 	constructor() {
 		this.cacheInvalidation = new CacheInvalidationService();
 	}
 
+	/**
+	 * Private instance of the database.
+	 */
+	private gpPool = gpPool;
+
+	/**
+	 * Public instance of the cache invalidation service.
+	 */
 	public cacheInvalidation;
 }
 
