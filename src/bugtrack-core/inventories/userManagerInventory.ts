d797f30de72d1c9f572d16f554819a282468ee94
John Shrubb <john-shrubb@users.noreply.github.com>
2024-06-03 23:33:28 +0100

    Add an authentication function to the user management inventory.

--- src/bugtrack-core/inventories/userManagerInventory.ts
+++ src/bugtrack-core/inventories/userManagerInventory.ts
@@ -7,6 +7,7 @@ import Session from '../types/session.js';
 import { QueryResult } from 'pg';
 import { possibleEvents } from '../services/cacheInvalidationService.js';
 import User from '../types/user.js';
+import generateID from '../helperFunctions/genID.js';
 
 interface sessionRowStructure {
 	sessiontoken : string;
@@ -190,6 +191,81 @@ class UserManagerInventory {
 
 		this.bgCore.cacheInvalidation.notifyUpdate(possibleEvents.session, session.id);
 	}
+
+	/**
+	 * Attempt to log in a user. On the API side, it may be advisable to artificially
+	 * delay responses to login attempts.
+	 * @param user The user object being authenticated.
+	 * @param password The password of the user.
+	 * @returns Returns either a token or an error.
+	 */
+
+	public async authenticateUser(
+		user : User, password : string, userAgent : string, ipAddress : string
+	): Promise<string | Error> {
+		// Check user exists.
+		if (!this.bgCore.userInventory.getUserByID(user.id)) {
+			throw new Error('Attempted to authenticate non existent user.', {
+				cause: user,
+			});
+		}
+
+		// Interface for password and salt.
+		interface passSaltStruct {
+			pass: string;
+			salt: string;
+		}
+
+		// Grab user data from database.
+		// This class avoids holding this data in memory for longer than required to
+		// prevent security where a user may get access to a cache of hashed passwords.
+		const userData : passSaltStruct = (await umPool.query('SELECT pass, salt FROM users WHERE userid=$1;', [user.id]).catch((reason) => {
+			// Catch error incase DB query goes wrong.
+			throw Error('Something has gone wrong with fetching user authentication details. User that exists in cache not present in database.', {
+				cause: {
+					userObject: user,
+					reason: reason,
+				},
+			});
+		})).rows[0]; // The first row will be correct.
+
+		const passwordCorrect =
+			await bcrypt.hash(password, userData.salt) === userData.pass;
+		
+		// Insert a record of the login attempt to the database.
+		await umPool.query('INSERT INTO loginattempts (userid, successful, ipaddress, useragent) VALUES ($1, $2, $3, $4);',
+		[user.id, passwordCorrect, ipAddress, userAgent] // With the parameters.
+		);
+
+		if (passwordCorrect) {
+			const token = await bcrypt.hash(crypto.randomUUID(), 30);
+			const id = generateID();
+
+			await umPool.query(
+				'INSERT INTO sessions (sessiontoken, userid, useragent, issuedon, expireson, sessionid) VALUES ($1, $2, $3, $4, $5, $6);',
+				[
+					token,
+					user.id,
+					userAgent,
+					new Date(),
+					// Session life of 2 weeks.
+					new Date(Date.now() + (1000 * 60 * 60 * 24 * 14)),
+					id
+				]
+			);
+
+			this.bgCore.cacheInvalidation.notifyUpdate(possibleEvents.session, id);
+
+			return token;
+		} else {
+			// It wouldn't really make sense to throw an error for every single failed
+			// authentication attempt.
+			const error = new Error('Incorrect authentication details.');
+			return error;
+		}
+	}
+
+	// Create/Delete a user.
 }
 
 export default UserManagerInventory;
