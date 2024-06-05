2609261354886a3846a6429a85af6c5b48232d2a
John Shrubb <john-shrubb@users.noreply.github.com>
2024-06-05 23:23:13 +0100

    Add a no cache variant to get a user by their ID

--- src/bugtrack-core/inventories/userInventory.ts
+++ src/bugtrack-core/inventories/userInventory.ts
@@ -166,6 +166,58 @@ class UserInventory {
 	}
 
 	/**
+	 * The same as getUserByID() but queries the database instead of the cache.
+	 * Is intended to be as sem
+	 */
+
+	// Maximum line length requires the line to be formatted like this.
+	public async noCacheGetUserByID(
+		userID : string,
+		throwError = true
+	): Promise<User | null> {
+		// Format check the user ID.
+		if (!checkID(userID) && throwError) {
+			throw Error('Attempted to query for a user with invalid ID: ' + userID);
+		}
+
+		// Code ripped from callback function. All the more reason to make me want to
+		// implement a driver system later on.
+		// Grab the raw query for the user data.
+		const userDataRaw : QueryResult<userRowStruct> = await this.gpPool.query(
+			'SELECT userid, username, email, displayname, pfp, creationdate FROM usersgp WHERE userid=$1;',
+			[userID]
+		).catch(reason => {
+			const error = new Error(
+				`Error while attempting to query database for user ID: ${userID}.`
+			);
+			error.cause = reason || 'Unknown error during connection to PostgreSQL.';
+			throw error;
+		});
+
+		// Check if the user was found,
+		if (!userDataRaw.rowCount) {
+			return null;
+		}
+
+		// Grab the user data.
+		const userData = userDataRaw.rows[0];
+
+		// Construct a user object to return.
+		const user = new User(
+			this.bugtrackCore,
+			userData.userid,
+			userData.username,
+			userData.email,
+			userData.displayname,
+			userData.pfp,
+			userData.creationDate,
+		);
+
+		// Return contrcuted user object.
+		return user;
+	}
+
+	/**
 	 * Attempt to find a user and get them by their email address. This function is less
 	 * performant than getUserByID() and should not be preferred.
 	 * 
