3e4e3df1098b18009d67b02ac26119892ed6ee59
John Shrubb <john-shrubb@users.noreply.github.com>
2024-06-12 19:52:33 +0100

    Fix minor spacing issue.

--- src/bugtrack-core/types/projectMember.ts
+++ src/bugtrack-core/types/projectMember.ts
@@ -11,7 +11,7 @@ import User from './user.js';
 
 class ProjectMember {
 	constructor(
-		bgCore : BugtrackCore,
+		bgCore       : BugtrackCore,
 		memberID     : string,
 		user         : User,
 		project      : Project,
