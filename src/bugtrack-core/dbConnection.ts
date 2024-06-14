a312ebf7d9457f1a2808654cc160c9c43f2dd9e6
John Shrubb <john-shrubb@users.noreply.github.com>
2024-06-14 14:15:04 +0100

    Add comments to dbConnection.ts

--- src/bugtrack-core/dbConnection.ts
+++ src/bugtrack-core/dbConnection.ts
@@ -1,10 +1,16 @@
 import pg from 'pg';
 
+// Pull in some environment variables
+
 const config = {
 	host: process.env['HOST'],
 	port: Number(process.env['PORT']),
 	database: process.env['DBNAME'],
 };
+
+// User Manager Pool.
+// Connects with the user manager role which can access passwords, salts, sessions and
+// login attempts.
 
 const umPool = await new pg.Pool({
 	host: config.host,
@@ -13,6 +19,10 @@ const umPool = await new pg.Pool({
 	user: process.env['UM-LOGIN'],
 	password: process.env['UM-PASSWORD'],
 }).connect();
+
+// General Purpose Pool.
+// Connects with the general purpose role which can access all other tables with limited
+// access to the users table (Passwords and salts omitted).
 
 const gpPool = await new pg.Pool({
 	host: config.host,
